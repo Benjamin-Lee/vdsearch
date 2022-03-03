4b9394fe309b65e6f4716fb7fe68ca8fd9c2cd4a
Benjamin Lee <benjamindlee@me.com>
2022-03-03 12:58:07 -0600

    Integrate clustering and caching into easy search

--- vdsearch/commands/cluster.py
+++ vdsearch/commands/cluster.py
@@ -1,33 +1,54 @@
 import logging
-from pathlib import Path
 import subprocess
-from rich.console import Console
+from pathlib import Path
 
-from vdsearch.types import FASTA
+import click
+import typer
+
+from vdsearch.types import FASTA, Threads
+from vdsearch.utils import check_executable_exists, typer_unpacker
 
 
-def cluster(fasta: Path = FASTA):
+@typer_unpacker
+def cluster(
+    fasta: Path = FASTA,
+    tmpdir: Path = typer.Option(
+        Path("tmp"),
+        help="Path to temporary directory to use for intermediate files",
+    ),
+    threads=Threads,
+):
     """Cluster circular sequences."""
+    check_executable_exists("mmseqs")
+
     logfile = Path("mmseqs.log.txt")
-    console = Console()
-    with console.status(f"Clustering [cyan]{str(fasta)}[/]"):
-        try:
-            subprocess.run(
-                f"seqkit concat --quiet {fasta} {fasta} | "
-                " mmseqs easy-cluster "
-                "--min-seq-id 0.75  "
-                "--min-aln-len 100 "
-                "--seq-id-mode 2 "
-                "--cov-mode 0 "
-                "-e 0.0001 "
-                "-c 0.75 "
-                "-s 7.5 "
-                "--cluster-mode 1 "
-                f"{fasta} clu tmp > {logfile}",
-                shell=True,
-                check=True,
-            )
-            console.log("[green]:heavy_check_mark:[/] Done clustering.")
-        except subprocess.CalledProcessError as e:
-            logging.error(f"Command failed with exit code {e.returncode}")
-            logging.error(f"Command output: '{e.output.decode('utf-8').rstrip()}'")
+
+    logging.info("Clustering...")
+    command = (
+        # f"seqkit concat --quiet {fasta} {fasta} | "
+        "mmseqs easy-cluster "
+        "--min-seq-id 0.75 "
+        "--min-aln-len 100 "
+        "--seq-id-mode 2 "
+        "--cov-mode 0 "
+        "-e 0.0001 "
+        "-c 0.75 "
+        "-s 7.5 "
+        "--cluster-mode 1 "
+        f"--threads {threads} "
+        f"{fasta} clu {tmpdir} "
+        f"> {logfile}"
+    )
+    logging.debug(f"{command=}")
+    try:
+        subprocess.run(
+            command,
+            shell=True,
+            check=True,
+        )
+        logging.done("Done clustering.")  # type: ignore
+    except subprocess.CalledProcessError as e:
+        logging.error(f"MMseqs failed with exit code {e.returncode}")
+        if e.output:
+            logging.error(f"MMseqs output: '{e.output.decode('utf-8').rstrip()}'")
+        raise click.ClickException("MMseqs failed. See logs for error messages.")
--- vdsearch/commands/easy_search.py
+++ vdsearch/commands/easy_search.py
@@ -1,6 +1,9 @@
 import logging
 from pathlib import Path
+import shutil
 from turtle import title
+import click
+import pandas as pd
 import rich
 from rich.console import Console
 
@@ -24,6 +27,7 @@ from vdsearch.nim import write_seqs as ws
 
 def easy_search(
     fasta: Path = FASTA,
+    outdir: Path = typer.Option(Path("."), file_okay=False, dir_okay=True),
     reference_db: Path = typer.Option(
         None,
         help="Path to FASTA-formatted reference viroid database. If none is provided, the latest ViroidDB will be used.",
@@ -32,6 +36,11 @@ def easy_search(
     ),
     reference_cms: Path = ReferenceCms,
     threads: int = Threads,
+    force: bool = typer.Option(
+        False,
+        help="Force running even if lockfile is present. Internal and inadvisable for production.",
+        hidden=True,
+    ),
 ):
     """
     Search for viroid-like sequences.
@@ -54,7 +63,22 @@ def easy_search(
     check_executable_exists("seqkit")
     check_executable_exists("cmsearch")
     check_executable_exists("mmseqs")
-    logging.debug("All needed tools exist.")
+
+    logging.debug("Making output directory...")
+    outdir.mkdir(parents=True, exist_ok=True)
+
+    # add a lock to the outdir to prevent parallel runs or incomplete data.
+    lockfile = outdir / ".vdsearch.lock"
+    if lockfile.exists() and not force:
+        raise click.ClickException(
+            f"Lockfile {lockfile} exists in the output directory.\n\n"
+            "Probably, a previous run is still in progress or has failed. "
+            f"If it has failed, data in {outdir} may be corrupt or incomplete. "
+            "Please wait for the previous run to finish or delete the lockfile if you are sure the data is not corrupt.\n\n"
+            f"To delete the lockfile, run:\n\n\trm {lockfile}"
+        )
+    lockfile.touch()
+    logging.debug("Directory locked.")
 
     logging.info(f"Beginning search for viroid-like RNAs using {threads} threads...")
     # if not reference_db:
@@ -64,35 +88,64 @@ def easy_search(
     #     download.download_cms()
 
     # run cirit/rotcanon
-    circs = Path(f"01.{fasta.stem}.circs.fasta")
-    find_circs(fasta, circs, canonicalize=True)
+    circs = outdir / Path(f"01.{fasta.stem}.circs.fasta")
+    if not circs.exists():
+        find_circs(fasta, circs, canonicalize=True)
+    else:
+        logging.warning("CircRNAs already found. Skipping.")
 
     # run dedup using seqkit
-    deduped_circs = Path(f"03.{fasta.stem}.deduped.fasta")
-    dedup(circs, deduped_circs, threads=threads)
+    deduped_circs = outdir / Path(f"03.{fasta.stem}.deduped.fasta")
+    if not deduped_circs.exists():
+        dedup(circs, deduped_circs, threads=threads)
+    else:
+        logging.warning("CircRNAs already deduplicated. Skipping.")
 
     # run infernal
-    cmsearch_output = Path(f"04.{fasta.stem}.infernal.out")
-    cmsearch_tsv = Path(f"04.{fasta.stem}.infernal.tsv")
-    infernal(
-        deduped_circs,
-        output=cmsearch_output,
-        output_tsv=cmsearch_tsv,
-        reference_cms=reference_cms,
-        threads=threads,
-        cmscan=False,
-    )
+    cmsearch_output = outdir / Path(f"04.{fasta.stem}.infernal.out")
+    cmsearch_tsv = outdir / Path(f"04.{fasta.stem}.infernal.tsv")
+    if not cmsearch_output.exists() and not cmsearch_tsv.exists():
+        infernal(
+            deduped_circs,
+            output=cmsearch_output,
+            output_tsv=cmsearch_tsv,
+            reference_cms=reference_cms,
+            threads=threads,
+            cmscan=False,
+        )
+    else:
+        logging.warning("Infernal already run. Skipping.")
 
     # find the viroids in the infernal output
-    ribozymes = ribozyme_filter(cmsearch_tsv, cm_file=reference_cms)
-    rz_seqs = Path(f"05.{fasta.stem}.ribozymes.fasta")
-    logging.info("Outputting viroid-like sequences...")
-    ws.write_seqs(
-        str(deduped_circs),
-        str(rz_seqs),
-        ribozymes["ribozy_likes"].seq_id.tolist(),
-    )
-    logging.done(f"Wrote to {rz_seqs}")  # type: ignore
+    rz_seqs = outdir / Path(f"05.{fasta.stem}.ribozymes.fasta")
+    if not rz_seqs.exists():
+        ribozymes = ribozyme_filter(cmsearch_tsv, cm_file=reference_cms)
+        logging.info("Outputting viroid-like sequences...")
+        ws.write_seqs(
+            str(deduped_circs),
+            str(rz_seqs),
+            ribozymes["ribozy_likes"].seq_id.tolist(),
+        )
+        logging.done(f"Wrote to {rz_seqs}")  # type: ignore
+    else:
+        logging.warning("Viroid-like sequences already found. Skipping.")
+
+    # run clustering
+    mmseqs_tmpdir = Path("mmseqstmp")
+    cluster_tsv = outdir / Path(f"06.{fasta.stem}.cluster.tsv")
+    if not cluster_tsv.exists():
+        cluster(rz_seqs, tmpdir=mmseqs_tmpdir, threads=threads)
+        Path("clu_cluster.tsv").rename(cluster_tsv)
+        logging.debug("Cleaning up temporary files from clustering...")
+        shutil.rmtree(mmseqs_tmpdir)
+    else:
+        logging.warning("Clustering already run. Skipping.")
+
+    # report on the clustering results
+    unique_cluster_count = pd.read_csv(
+        cluster_tsv, sep="\t", names=["cluster_id", "seq_id"]
+    ).cluster_id.nunique()
+    logging.done(f"{unique_cluster_count} viroid-like clusters found")  # type: ignore
 
     # # run mmseqs
     # mmseqs(fasta)
@@ -121,3 +174,7 @@ If you use these results in your research, please cite:
         "[dim]Brought to you by: [cyan bold]NIH/NLM/NCBI[/], [blue bold]University of Oxford[/], and [bold]Tel Aviv University[/][/dim]",
         "\n",
     )
+
+    # remove the lock
+    lockfile.unlink()
+    logging.debug("Lockfile removed.")
