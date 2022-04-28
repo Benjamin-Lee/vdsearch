e39c5a5d179d67a2f992529248e2468f906f90e3
Benjamin Lee <benjamindlee@me.com>
2022-04-28 17:41:45 -0400

    split desc line whitespace when writing seqs out to conform to infernal parsing

--- vdsearch/nim/write_seqs.nim
+++ vdsearch/nim/write_seqs.nim
@@ -1,13 +1,15 @@
 import bioseq
 import nimpy
-import std/[sets, tables, os]
+import std/[sets, tables, os, strutils]
 
 proc write_seqs*(infile: string, outfile: string, ids: seq[string]): void {.exportpy.} =
   let idSet = toHashSet(ids)
   let outfileFile = open(outfile, fmWrite) # the output file as an opend File object
   defer: outfileFile.close()
   for record in readFasta[Dna](infile):
-    if record.description in idSet:
+    # Infernal only reports the ID, not the full header so we have to parse it
+    # I'm not a fan of this trying to guess FASTA header format
+    if record.description.splitWhitespace()[0] in idSet:
       outfileFile.writeLine(record.asFasta())
 
 proc write_clusters*(infile: string, outdir: string, mapping: Table[string, string], clusters: seq[string]): void {.exportpy.} =
